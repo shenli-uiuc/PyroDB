688e17d8b4e42732258aa56d17cc1788d443653c
Shen Li <shenli3@illinois.edu>
2014-07-29 21:55:01 -0400

    ....

--- test/hbase-0.99.0-shen/scripts/pssh_setup_all.sh
+++
@@ -1,9 +0,0 @@
-# execute on tareka05
-rm /home/tarek/shenli3/software/src/hbase-0.99.0-shen.tar.gz
-cp ../../hbase-0.99.0-shen.tar.gz /home/tarek/shenli3/software/src/
-mkdir -p /home/tarek/shenli3/script/pssh/hbase/msgerr
-mkdir -p /home/tarek/shenli3/script/pssh/hbase/msgout
-cp ./setup_hbase_node.sh /home/tarek/shenli3/script/pssh/hbase/
-pssh -h ../conf/regionservers -e /home/tarek/shenli3/script/pssh/hbase/msgerr -o /home/tarek/shenli3/script/pssh/hbase/msgout /home/shenli3/script/pssh/hbase/setup_hbase_node.sh
-pssh -h ../conf/master -e /home/tarek/shenli3/script/pssh/hbase/msgerr -o /home/tarek/shenli3/script/pssh/hbase/msgout /home/shenli3/script/pssh/hbase/setup_hbase_node.sh
-pssh -h ../conf/zookeepers -e /home/tarek/shenli3/script/pssh/hbase/msgerr -o /home/tarek/shenli3/script/pssh/hbase/msgout /home/shenli3/script/pssh/hbase/setup_hbase_node.sh
--- test/hbase-0.99.0-shen/scripts/reboot.sh
+++
@@ -1,2 +0,0 @@
-echo "$1"
-echo -e "$1" | ssh -t -t `hostname` sudo -S reboot
